13e8ca54770de73abd27bb983188d27895d75571
Claire Han <cjh286@cornell.edu>
2020-10-09 21:53:55 -0400

    button component

--- static/js/app.jsx
+++ static/js/app.jsx
@@ -1,6 +1,12 @@
 import React from "react";
+import Button from "./components/button";
 export default class App extends React.Component {
   render() {
-    return <p> Hello React!</p>;
+    return (
+      <>
+        <p> Hello React!</p>
+        <Button />
+      </>
+    );
   }
 }
